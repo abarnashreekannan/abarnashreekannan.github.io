37e5b88a3facef28a1bea8ba10f2905b1df33ea9
abarnashreekannan <abarnashreekannan@gmail.com>
2026-03-04 15:36:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple smooth scroll enhancement (optional future space for animations)
+
+document.querySelectorAll("nav a").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
